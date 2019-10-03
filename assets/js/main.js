a6415449dc97d0820968b7e53627a3cebca32c93
Esteban Palacio <estipalacio@gmail.com>
2019-10-03 14:05:22 -0400

    Add new modal functionality to portfolio and aligned logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -173,3 +173,11 @@
 			});
 
 })(jQuery);
+
+
+// Modal
+
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
